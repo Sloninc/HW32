421efd898b45babd998e904d7ccd4cf480bcd863
Andrei Ruk <sloninc@mail.ru>
2023-08-14 13:36:02 +0300

    Добавлен метод WriteFileInfo.

--- Program.cs
+++ Program.cs
@@ -11,27 +11,15 @@ namespace HW32
             directoryInfo1.Create();
             DirectoryInfo directoryInfo2 = new DirectoryInfo(path2);
             directoryInfo2.Create();
-
-            var files = directoryInfo1.GetFiles();
-   
-            Console.WriteLine("Имя файла\t\tТекст из файла");
-            for(int i = 0; i < files.Length; i++)
-            {
-                string name = Path.GetFileNameWithoutExtension(path1 + $"\\File{i + 1}.txt");
-                using (FileStream fstream = File.OpenRead(path1 + $"\\File{i + 1}.txt"))
-                {
-                    // выделяем массив для считывания данных из файла
-                    byte[] buffer = new byte[fstream.Length];
-                    // считываем данные
-                    await fstream.ReadAsync(buffer, 0, buffer.Length);
-                    // декодируем байты в строку
-                    string textFromFile = Encoding.UTF8.GetString(buffer);
-                    Console.WriteLine($"{name}:\t\t{textFromFile}");
-                }
-            }
+            await CreateFileAndWrite(path1);
+            await CreateFileAndWrite(path2);
+            await WriteFilesAddition(directoryInfo1);
+            await WriteFilesAddition(directoryInfo2);
+            await WriteFileInfo(directoryInfo1);
+            await WriteFileInfo(directoryInfo2);
             Console.ReadLine();
         }
-        async Task CreateFileAndWrite(string directoryPath)
+        static async Task CreateFileAndWrite(string directoryPath)
         {
             for (int i = 0; i < 10; i++)
             {
@@ -39,7 +27,7 @@ namespace HW32
                 try
                 {
                     // запись в файл
-                    using (FileStream fstream = File.Create(directoryPath + $"\\File{i + 1}.txt"))
+                    using (FileStream fstream = File.Create(directoryPath + $"\\File_{(i + 1):D2}.txt"))
                     {
                         name = fstream.Name;
                         // преобразуем строку в байты
@@ -63,41 +51,91 @@ namespace HW32
             }
         }
 
-        async Task WriteFilesAddition(DirectoryInfo info)
+        static async Task WriteFilesAddition(DirectoryInfo info)
         {
-            FileInfo[] files = info.GetFiles();
-            foreach (var file in files)
+            try
             {
-                try
+                FileInfo[] files = info.GetFiles();
+                foreach (var file in files)
                 {
-                    // запись в файл
-                    using (FileStream fstream = file.Open(FileMode.Append))
+                    try
                     {
-                        if (fstream.CanWrite)
+                        // запись в файл
+                        using (FileStream fstream = file.Open(FileMode.Append))
                         {
-                            // преобразуем строку в байты
-                            byte[] buffer = Encoding.UTF8.GetBytes(" " + DateTime.Now.ToString());
-                            // запись массива байтов в файл
-                            await fstream.WriteAsync(buffer, 0, buffer.Length);
+                            if (fstream.CanWrite)
+                            {
+                                // преобразуем строку в байты
+                                byte[] buffer = Encoding.UTF8.GetBytes(" " + DateTime.Now.ToString());
+                                // запись массива байтов в файл
+                                await fstream.WriteAsync(buffer, 0, buffer.Length);
+                            }
                         }
                     }
+                    catch (FileNotFoundException)
+                    {
+                        Console.WriteLine($"Файл {file.Name} не найден.");
+                    }
+                    catch (UnauthorizedAccessException)
+                    {
+                        Console.WriteLine($"Файл {file.Name} доступен только для чтения или является каталогом.");
+                    }
+                    catch (IOException)
+                    {
+                        Console.WriteLine("Файл уже открыт.");
+                    }
                 }
-                catch (FileNotFoundException)
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("Указан недопустимый путь");
+            }
+
+        }
+
+        static async Task WriteFileInfo(DirectoryInfo info)
+        {
+            Console.WriteLine($"Имя файла:\t\tСодержимое файла");
+            try
+            {
+                var files = info.GetFiles();
+                //for (int i = 0; i < files.Length; i++)
+                foreach(var file in files)
                 {
-                    Console.WriteLine($"Файл {file.Name} не найден.");
+                    string name = "";
+                    try
+                    {
+                        // запись в файл
+                        name = file.Name;
+                        using (FileStream fstream = File.OpenRead(file.FullName))
+                        {
+                            // выделяем массив для считывания данных из файла
+                            byte[] buffer = new byte[fstream.Length];
+                            // считываем данные
+                            await fstream.ReadAsync(buffer, 0, buffer.Length);
+                            // декодируем байты в строку
+                            string textFromFile = Encoding.UTF8.GetString(buffer);
+                            Console.WriteLine($"{name}:\t\t{textFromFile}");
+                        }
+                    }
+                    catch (UnauthorizedAccessException)
+                    {
+                        Console.WriteLine($"Файл {name} доступен только для чтения или является каталогом.");
+                    }
+                    catch (FileNotFoundException)
+                    {
+                        Console.WriteLine($"Файл {name} не найден.");
+                    }
+                    catch (IOException ex)
+                    {
+                        //foreach(Exception exception in ex.InnerException)
+                        Console.WriteLine($"Файл {name} уже открыт.");
+                    }
                 }
-                catch (UnauthorizedAccessException)
-                {
-                    Console.WriteLine($"Файл {file.Name} доступен только для чтения или является каталогом.");
-                }
-                catch (DirectoryNotFoundException)
-                {
-                    Console.WriteLine("Указан недопустимый путь");
-                }
-                catch (IOException)
-                {
-                    Console.WriteLine("Файл уже открыт.");
-                }
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("Указан недопустимый путь");
             }
         }
     }
